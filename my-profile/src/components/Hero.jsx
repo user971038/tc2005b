3c0241162cfd23bad34f7ad7ba883e6027ab2939
user971038 <A00226997@tec.mx>
2026-04-09 06:10:38 -0600

    more updates

--- my-profile/src/components/Hero.jsx
+++ my-profile/src/components/Hero.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router'
 const Hero = () => {
   return (
     <div>
-        <p className="main-text">
-            Welcome. {/*<span className="secondary-text">Digital Artifacts</span>*/}
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+        <div className="main-text-container">
+            <p className="main-text">
+                Welcome. {/*<span className="secondary-text">Digital Artifacts</span>*/}
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
         <div className="btn-container">
             <button className="btn">
                 <Link to="/Projects">View Projects</Link>
