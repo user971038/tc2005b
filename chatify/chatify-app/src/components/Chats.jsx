c79befe5d23dd1507f703f5252496e14c56916d3
user971038 <A00226997@tec.mx>
2026-04-20 05:59:30 -0600

    tutorial pt 2

--- chatify/chatify-app/src/components/Chats.jsx
+++ chatify/chatify-app/src/components/Chats.jsx
@@ -3,16 +3,25 @@ import './Components.css'
 
 const Chats = () => {
 
+    const [messages, setMessages] = useState([]);
+
     useEffect(() => {
         socket.on('chat message', (msg) => {
             console.log("Mensaje desde Server:", msg);
+            setMessage((prev) =>[...prev, msg])
         });
+        return () => {
+            socket.off('chat message')
+        };
     }, []);
 
     return (
         <div class="container" id="chats-container">
-            <div></div>
-            <input id="chatbox"></input>
+            {messages?.map((m) => (
+                <p>{m}</p>
+            ))};
+            {/*<div></div>
+            <input id="chatbox"></input>*/}
         </div>
     );
 };
