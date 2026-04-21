e12b03f5078119a44bb53a2fc55d958b04a2a869
user971038 <A00226997@tec.mx>
2026-04-21 06:22:40 -0600

    finished portfolio (can be changed)

--- my-profile/src/pages/About.jsx
+++ my-profile/src/pages/About.jsx
@@ -7,16 +7,28 @@ const About = () => {
     <div className="body">
       <Navbar />
       <h1 style={{ color: '#ff00e5' }}>About <span style={{ color: 'white' }}>Me</span></h1>
-      <div className="content-container">
-        <div className="content-row" id="about-left">
-          <img style={{ width: '79%' }} src="https://plus.unsplash.com/premium_photo-1664299749481-ac8dc8b49754?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="lit me" />
-          <h3>A picture of me (2022).</h3>
-          <p>Texto</p>
+      <div className="content-container" id="container-about">
+        <div className="content-about" id="about-left">
+          <img style={{ width: '79%' }} src="/../public/myself_bw.png" alt="lit me" />
+          <h3>A picture of a cat (2022).</h3>
         </div>
-        <div className="content-row" id="about-right">
+        <div className="content-about" id="about-right">
           <h3 style={{ color: '#00f0ff' }}>Who am I?</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi laoreet consectetur turpis, non tempus sem volutpat finibus. Curabitur sed dui eleifend, pellentesque augue et, elementum neque. Pellentesque euismod ante sit amet fringilla commodo. Donec justo nisl, convallis aliquet magna eget, viverra aliquam risus. Sed ac iaculis nisl. Sed at purus varius, dictum mauris vel, egestas nunc. Etiam sit amet mollis velit. Suspendisse id lacus feugiat, mollis risus at, lobortis leo. Quisque vel dui vel neque mollis eleifend et eu urna.</p>
-          <p>Cras quis erat lorem. Sed convallis, massa in convallis pharetra, augue sem porttitor risus, nec finibus justo leo eu justo. Mauris nec consequat dolor, vel scelerisque massa. Phasellus imperdiet orci est, vel aliquet velit cursus tincidunt. Aliquam ultrices rhoncus lectus, in laoreet diam convallis nec. Etiam rutrum mi sit amet arcu condimentum ultricies. Nunc vehicula arcu id elit consequat, quis ornare velit iaculis.</p>
+          <p>I'm a computer science student driven by a mix of curiosity and a love for 
+            problem-solving. My journey in tech is defined by a desire to understand how 
+            complex systems work and an even greater passion for building tools that make 
+            life a little easier. Whether I’m diving into the logic of a new backend 
+            framework or fine-tuning the details of a user interface, I enjoy the 
+            challenge of turning abstract ideas into functional, clean code. Outside of my 
+            coursework, I’m constantly looking to broaden my perspective by exploring 
+            emerging technologies and collaborating on projects that push me to think 
+            outside the box. I'm excited to continue growing as a developer and to 
+            contribute to meaningful solutions in an ever-evolving digital landscape.</p>
+          <h3 style={{ color: '#00f0ff' }}>My abilities</h3>
+          <p>Languages: Python, C#/C++, JavaScript.</p>
+          <p>Web Development: HTML5/CSS3, React, Node.js.</p>
+          <p>Data & Databases: PostgreSQL, Firebase; Data Structures and Algorithms.</p>
+          <p>Tools & Platforms: Git/GitHub, Linux/Unix Command Line, VS Code.</p>
         </div>
       </div>
     </div>
--- my-profile/src/pages/Projects.jsx
+++ my-profile/src/pages/Projects.jsx
@@ -7,9 +7,34 @@ const Projects = () => {
     <div className="body">
       <Navbar />
       <h1 style={{ color: '#00f0ff' }}>Projects</h1>
-      <p className="description-text">A peek to some of the stuff I've done.</p>
-      <div className="content-container">
+      <p className="description-text">A peek into some of the stuff I've done.</p>
+      <div className="content-container" id="container-projects">
+        <div className="content-projects">
+          <h3>Graphic Design Portfolio</h3>
+          <p className="date">May 2024</p>
+          <p><span style={{  fontWeight: 'bold' }}>Description: </span>
+          Development of several web portfolios for different graphic designers.</p>
+          <p><span style={{  fontWeight: 'bold' }}>Abilities & Tools: </span>
+          HTML5, CSS, WordPress, Hostinger.</p>
+        </div>
 
+        <div className="content-projects">
+          <h3>Snakes & Ladders Game</h3>
+          <p className="date">April 2025</p>
+          <p><span style={{  fontWeight: 'bold' }}>Description: </span>
+          Creation, testing and publishing of a text-based game of Snakes & Ladders.</p>
+          <p><span style={{  fontWeight: 'bold' }}>Abilities & Tools: </span>
+          C++, VSCode, GitHub.</p>
+        </div>
+
+        <div className="content-projects">
+          <h3>Chatify</h3>
+          <p className="date">April 2026</p>
+          <p><span style={{  fontWeight: 'bold' }}>Description: </span>
+          Design and testing of a web-based texting app. Currently still in development.</p>
+          <p><span style={{  fontWeight: 'bold' }}>Abilities & Tools: </span>
+          HTML, CSS, React, Railway, Postgress.</p>
+        </div>
       </div>
     </div>
   )
